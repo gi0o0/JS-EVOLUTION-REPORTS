0aea220bfdbb0dab2f4a69158959388eaa0ae692
gi0o0 <martinzodiacal1000@gmail.com>
2022-12-20 16:22:14 -0500

    add reports v01

---
+++ reportes2/src/reportes/llamar.java
@@ -0,0 +1,16 @@
+
+package reportes;
+
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class llamar {
+
+    public static void main(String args[]) {
+        
+     
+    }
+
+}
